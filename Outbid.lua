9a6e4ddefadc9df7174b976db86aa13beefb9d79
Joshua Shaffer <joshua.shaffer@outlook.com>
2014-11-23 01:34:18 -0500

    Sell order support

--- Outbid.lua
+++ Outbid.lua
@@ -24,7 +24,7 @@ function Outbid:OnInitialize()
 
 	self.orders = nil
 	self.orderListWindow = nil
-
+	
 	Apollo.RegisterEventHandler("CommodityInfoResults", "OnCommodityInfoResults", self)
 end
 
@@ -34,14 +34,6 @@ end
 function Outbid:OnDocLoaded()
 
 	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
-	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "OutbidForm", nil, self)
-		if self.wndMain == nil then
-			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
-			return
-		end
-		
-	    self.wndMain:Show(false, true)
-
 		self:Hook(Apollo.GetAddon("MarketplaceListings"), "OnOwnedCommodityOrders")
 		self:PostHook(Apollo.GetAddon("MarketplaceListings"), "BuildCommodityOrder")
 	end
@@ -56,6 +48,29 @@ end
 function Outbid:OnCommodityInfoResults(nItemId, tStats, tOrders)
 	if not self.orders or not self.orderListWindow or not self.orderListWindow:IsValid() then
 		return
+	end
+	
+	if self.oldSellOrder and self.readyToResell then
+		if self.oldSellOrder:GetItem():GetItemId() == nItemId then
+			local topPrice = tStats.arSellOrderPrices[1].monPrice
+			self.sellDialog = Apollo.LoadForm(self.xmlDoc, "ResellConfirm", nil, self)
+			local sellButton = self.sellDialog:FindChild("SellButton")
+			
+			local newPrice = Money.new()
+			if self:HaveMatchingOrderPrice(nItemId, topPrice) then
+				newPrice:SetAmount(topPrice:GetAmount())
+			else
+				newPrice:SetAmount(topPrice:GetAmount() - 1)
+			end
+			
+			self:CreateOrderCommand("sell", sellButton, nItemId, newPrice, self.oldSellOrder:GetCount())
+			
+			self.sellDialog:FindChild("UnitPrice"):SetAmount(newPrice:GetAmount(), true)
+			self.sellDialog:Invoke()
+			
+			self.oldSellOrder = nil
+			self.readyToResell = false
+		end
 	end
 
 	for idx, order in pairs(self.orders) do
@@ -79,10 +94,20 @@ function Outbid:OnCommodityInfoResults(nItemId, tStats, tOrders)
 					else
 						newPrice:SetAmount(topPrice:GetAmount() + 1)
 					end
-					self:CreateBuyOrderCommand(newButton, nItemId, newPrice, order:GetCount())
+					self:CreateOrderCommand("buy", newButton, nItemId, newPrice, order:GetCount())
 				end
 			else
-				--topPrice = tStats.arSellOrderPrices[1]
+				local topPrice = tStats.arSellOrderPrices[1].monPrice --Top 1 is first index
+				if orderPrice:GetAmount() > topPrice:GetAmount() then					
+					local listItemPanel = self.orderListWindow:FindChildByUserData(order)
+					local oldButton = listItemPanel:FindChild("RelistSellButton")
+					if oldButton then
+						oldButton:Destroy()
+					end
+					
+					local newButton = Apollo.LoadForm(self.xmlDoc, "RelistSellButton", listItemPanel, self)
+					newButton:SetData(order)
+				end
 			end
 		end
 	end
@@ -92,9 +117,10 @@ function Outbid:OnOwnedCommodityOrders(luaCaller, tOrders)
 	self.orders = tOrders
 end
 
-function Outbid:CreateBuyOrderCommand(button, itemId, price, amount)
+function Outbid:CreateOrderCommand(orderType, button, itemId, price, amount)
 	--local orderNew = bBuyTab and CommodityOrder.newBuyOrder(tCurrItem:GetItemId()) or CommodityOrder.newSellOrder(tCurrItem:GetItemId())
-	local orderNew = CommodityOrder.newBuyOrder(itemId)
+	
+	local orderNew = orderType == "buy" and CommodityOrder.newBuyOrder(itemId) or CommodityOrder.newSellOrder(itemId)
 	if price and amount then
 		orderNew:SetCount(amount)
 		orderNew:SetPrices(price)
@@ -129,5 +155,66 @@ function Outbid:SubmittedBuyOrder( wndHandler, wndControl, bSuccess )
 		return
 	end
 	order:Cancel()
+	
+	--self:BuildTimer("BuyMailTimer")
+end
+
+function Outbid:RelistSellClick( wndHandler, wndControl, eMouseButton )
+	local order = wndHandler:GetData()
+	if not order then
+		return
+	end
+	order:Cancel()
+	self.oldSellOrder = order
+	self.readyToResell = false
+	self:BuildTimer("SellMailTimer")
+	--MailSystemLib.GetInbox()[1]:GetMessageInfo().arAttachments
+end
+
+function Outbid:BuildTimer(timerFunc)
+	if self.timer then
+		self.timer:Stop()
+		self.timer = nil
+	end
+	self.timer = ApolloTimer.Create(0.5, true, timerFunc, self)
+end
+
+function Outbid:SellMailTimer()
+	local mails = MailSystemLib.GetInbox()
+	
+	if not self.oldSellOrder then
+		self.timer:Stop()
+	end
+	
+	local oldItemId = self.oldSellOrder:GetItem():GetItemId()
+	
+	for idx, mail in pairs(mails) do
+		if self:IsItemAttached(mail, oldItemId) then
+			mail:TakeAllAttachments()
+			self.timer:Stop()
+			self.readyToResell = true
+			MarketplaceLib.RequestCommodityInfo(oldItemId) --Re-trigger a market check to relist
+		end
+	end
+end
+
+function Outbid:IsItemAttached(mail, itemId)
+	attachements = mail:GetMessageInfo().arAttachments
+	for idx, attachment in pairs(attachements) do
+		if attachment.itemAttached:GetItemId() == itemId then
+			return true
+		end
+	end
+	return false
+end
+
+function Outbid:SellCancelClick( wndHandler, wndControl, eMouseButton )
+	self.sellDialog:Destroy()
+	self.sellDialog = nil
+end
+
+function Outbid:SellOrderSubmitted( wndHandler, wndControl, bSuccess )
+	self.sellDialog:Destroy()
+	self.sellDialog = nil
 end
 
